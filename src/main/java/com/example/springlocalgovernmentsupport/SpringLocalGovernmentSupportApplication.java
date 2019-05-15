753293c7ad89457e80401d93b296628a8869a5b7
Estel Seong <estel.castle@gmail.com>
2019-05-15 22:22:36 +0900

    initial commit.

---
+++ src/main/java/com/example/springlocalgovernmentsupport/SpringLocalGovernmentSupportApplication.java
@@ -0,0 +1,13 @@
+package com.example.springlocalgovernmentsupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLocalGovernmentSupportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLocalGovernmentSupportApplication.class, args);
+    }
+
+}
